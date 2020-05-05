34687b7d3df5cedaf5d90df0b1044d8d5bd39079
Richard Hanolt <rhanolt@gmail.com>
2020-05-05 21:28:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,1 +1,15 @@
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|dwarf,index| puts "#{index+1}. #{dwarf}"}
+end
 
+def summon_captain_planet
+
+end
+
+def long_planeteer_calls
+
+end
+
+def find_the_cheese
+
+end
